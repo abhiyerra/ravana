be760104c4f0863f3aec3228669dbbd8a5842ee3
Abhi Yerra <ykabhinav@gmail.com>
2010-10-29 23:14:08 -0700

    Removed email and using login

--- db/migrate/20101024190319_devise_create_users.rb
+++ db/migrate/20101024190319_devise_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
+      t.database_authenticatable :null => false, :authentication_keys => [:login] 
       t.recoverable
       t.rememberable
       t.trackable
