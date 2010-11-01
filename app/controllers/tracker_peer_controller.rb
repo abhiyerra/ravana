ce4b9514466b7d217ba94bc7db09daa813617be7
Abhi Yerra <ykabhinav@gmail.com>
2010-11-01 09:03:59 -0700

    Added tracker uniqueness

--- app/controllers/tracker_peer_controller.rb
+++ app/controllers/tracker_peer_controller.rb
@@ -1,9 +1,21 @@
 class TrackerPeerController < ApplicationController
+  # Steps to verifying a tracker peer.
+  # 1. Display a form which asks for tacker_infomation.
+  # 2. Add that form to the database.
+  # 3. Verify the tracker.
+  #    a. Ask other trackers on our list for tracker credibility.
+  #    b. Verify user manually.
+  # 4. Once verified then send out new_torrents.
   def new
-    
+     
   end
 
   def create
+    @tracker_peer = TrackerPeer.new(params[:tracker_peer])
+    @tracker_peer.save
+  end
+
+  def manage
 
   end
 end
--- app/models/tracker_peer.rb
+++ app/models/tracker_peer.rb
@@ -1,5 +1,37 @@
 class TrackerPeer < ActiveRecord::Base
-  def send_new_torrent
+  
+  validates_uniqueness_of :name
+  validates_uniqueness_of :url
+  validates_uniqueness_of :callback_url
+ 
+  def verify_tracker_peer
+      # t.string :name
 
+      # t.string :url
+      # t.string :callback_url
+
+      # t.string :admin_email
+
+      # t.string :consumer_key
+      # t.string :consumer_secret_key
+      # t.string :comsumer_request_token_url
+      # t.string :consumer_access_token_url
+      # t.string :consumer_authorize_url
+
+      # t.boolean :is_allowed
+
+  end
+
+  def send_new_torrent(torrent)
+      TrackerPeer.all.each do |tracker_peer|
+          torrent_info = {
+              'name'             => torrent.name,
+              'description'      => torrent.description,
+              'info_hash'        => torrent.info_hash,
+              'torrent_file_url' => torrent.torrent.url
+          }
+          
+          RestRequest.post(tracker_peer.callback_url, torrent_info)
+      end
   end
 end
