40c9a946e0efa422810cd6612d05041efc5064fd
Abhi Yerra <ykabhinav@gmail.com>
2010-12-27 21:21:06 -0800

    Removed the tracker. Going to focus completely on the indexer side for right now.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-$CONFIG = YAML::load(File.join(Rails.root, 'config', 'ravana.yml'))
-
 # Initialize the rails application
 Tradings::Application.initialize!
--- test/unit/helpers/tracker_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TrackerHelperTest < ActionView::TestCase
-end
